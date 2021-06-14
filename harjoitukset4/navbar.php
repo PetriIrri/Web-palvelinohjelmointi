55c39c0b5a6df4c13dea10cf9d039c7a9a38c02a
PetriIrri <petri.irri@gmail.com>
2021-06-14 12:36:33 +0300

    Navbar for the application. Is used in every page.

---
+++ harjoitukset4/navbar.php
@@ -0,0 +1,13 @@
+<div style="text-align: right; margin-top: 10px;padding-bottom: 10px;">
+<span style="background-color: #ddd;">Cbook</span>
+<em>by QaD Solutions</em>
+</div>
+    [ <a href='cbook-list.php'>Listaa</a> ]
+    [ <a href='cbook-addform.php'>Lisää</a> ]
+    <br><br>
+    <form action="cbook-list.php" method="post">
+        <input type="text" name="searchterm" placeholder="search">
+        <input type="submit">
+    </form>
+
+<hr>
